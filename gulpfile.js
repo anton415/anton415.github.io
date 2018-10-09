0b9a64a5aec8368cd2fa0e380e15ce5fc5acfa68
Anton Serdyuchenko <anton415@gmail.com>
2018-10-09 21:36:18 +0300

    Install imagemin.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@
 var gulp = require(' gulp');
 var concat = require(' gulp-concat');
 var myth = require(' gulp-myth');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('styles', function() {
   return gulp.src('stylesheets/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
 });
