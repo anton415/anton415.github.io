7f99c524154f6396fbb0f20b8fbf319eb1721c9f
Anton Serdyuchenko <anton415@gmail.com>
2018-10-09 21:24:35 +0300

    Import Sass plugin.

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,9 @@
 var gulp = require(' gulp');
 var concat = require(' gulp-concat');
 var myth = require(' gulp-myth');
+
+gulp.task('styles', function() {
+  return gulp.src('stylesheets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'));
+});
