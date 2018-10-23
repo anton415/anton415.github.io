a981bb3a8e0acf368ea89c39fb1f59f44501dbc5
Anton Serdyuchenko <anton415@gmail.com>
2018-10-23 18:05:03 +0300

    Cahange title view's for desctop version.

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('watch', function() {
   gulp.watch('index.html').on('change', reload);
   gulp.watch('./app/blog/blog.html').on('change', reload);
   gulp.watch('./app/projects/projects.html').on('change', reload);
+  gulp.watch('./app/stylesheets/css/styles.css').on('change', reload);
   gulp.watch('./app/*').on('change', reload);
 });
 
