b1d342066a2c5273544ed9e0837c309059623869
Anton Serdyuchenko <anton415@gmail.com>
2018-11-01 20:56:00 +0300

    Add new menu to main page.

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,20 @@ gulp.task('browser-sync', function() {
 
 // Watch Task.
 gulp.task('watch', function() {
+  // HTML
   gulp.watch('index.html').on('change', reload);
   gulp.watch('./app/blog/blog.html').on('change', reload);
   gulp.watch('./app/projects/projects.html').on('change', reload);
   gulp.watch('./app/courses/courses.html').on('change', reload);
-  gulp.watch('./app/stylesheets/css/styles.css').on('change', reload);
   gulp.watch('./app/info/info.html').on('change', reload);
+
+  // CSS
+  gulp.watch('./app/stylesheets/css/styles.css').on('change', reload);
+  gulp.watch('./app/stylesheets/css/styles.css').on('change', reload);
+
+  // JS
+  gulp.watch('./app/script/include_footer.js').on('change', reload);
+  gulp.watch('./app/script/menu.js').on('change', reload);
 });
 
 gulp.task('default', ['browser-sync', 'watch']);
