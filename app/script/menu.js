3d9037a50c71a717d0b207dcf342ee4ab832435e
Anton Serdyuchenko <anton415@gmail.com>
2018-11-15 06:27:49 +0300

    New nav menu.

--- app/script/menu.js
+++ app/script/menu.js
@@ -1,9 +1,19 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
+// /* Open when someone clicks on the span element */
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+// }
+//
+// /* Close when someone clicks on the "x" symbol inside the overlay */
+// function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+// }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
