dc79f0797b0106d40960fba6bcfdaf086eb4a858
Anton Serdyuchenko <anton415@gmail.com>
2019-03-02 13:52:03 +0300

    Move Home's component to 'home' folder and show component on App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Menu from './menu/Menu'
+import Home from './home/Home'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Menu />
+        <Home />
       </div>
     );
   }
