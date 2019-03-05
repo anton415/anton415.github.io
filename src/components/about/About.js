76756338343b4568bf1db8c5c95d4b99ebfa177d
Anton Serdyuchenko <anton415@gmail.com>
2019-03-05 06:17:06 +0300

    Add work experience.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,7 +7,23 @@ class About extends Component {
       <div className="about">
         About page.
         <div className="card">
-          Card.
+          <p>Experience</p>
+          <p>Full stack developer</p>
+          <p>Central Bank of the Russian Federation</p>
+          Oct 2017 - Present
+          Moscow, Russian Federation
+          <p>
+            Since October 2017, I work in set of tasks "Accounting for the
+            application of measures of influence to shareholders (participants)
+            of KO and other persons". As part of this complex, I perform the
+            role of junior full stack developer. In the part of the front end
+            of development technologies are used: JSP, JS, CSS. In the back
+            end part: Java. Database: Oracle 10. The application runs on the
+            IBM WebSphere portal. I actively propagated the need to use JUnit
+            test, as a result of which I was entrusted to start covering the
+            application with tests. Coverage tests led to improved quality of
+            written code and increased reliability of the application.
+          </p>
         </div>
       </div>
     );
