5b45a322fbc994e84b5feae5d548a01664ba58ff
Anton Serdyuchenko <anton415@gmail.com>
2019-03-22 19:46:14 +0300

    Add semantic ui.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css'
 
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
 ReactDOM.render(<App />, document.getElementById('root'));
