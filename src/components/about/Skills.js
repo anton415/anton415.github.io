1d62c1344fe7858a799de7638b18211cca4de67c
Anton Serdyuchenko <anton415@gmail.com>
2019-03-24 20:04:37 +0300

    Add skills.

---
+++ src/components/about/Skills.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Skills extends Component {
+  render() {
+    return (
+      <div className="skills section">
+        <h3>Skills</h3>
+        <div className="card">
+          <span class="skill">React</span>
+          <span class="skill">Redux</span>
+          <span class="skill">React Native</span>
+          <span class="skill">Git</span>
+          <span class="skill">PWA</span>
+          <span class="skill">Java</span>
+          <span class="skill">SQL</span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Skills
