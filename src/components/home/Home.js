34536201353d3a56d9dc8c00bbd38ed4edf000c6
Anton Serdyuchenko <anton415@gmail.com>
2019-04-20 15:53:17 +0300

    Edit links.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,11 +14,23 @@ class Home extends Component {
           <Description />
           <br />
           You can download
-          <NavLink to={CVShort} target='_blank' download='SerdyuchenkoAN' title='SerdyuchenkoAN CV short' className='nav-link'> short </NavLink>
+          <NavLink
+            to={CVShort}
+            target='_blank'
+            download='SerdyuchenkoAN'
+            title='Download SerdyuchenkoAN.pdf CV short version'
+            className='nav-link'
+          >  short  </NavLink>
           or
-          <NavLink to={CVLong} target='_blank' download='SerdyuchenkoAN' title='SerdyuchenkoAN CV long' className='nav-link'> long </NavLink>
+          <NavLink
+            to={CVLong}
+            target='_blank'
+            download='SerdyuchenkoAN'
+            title='Download SerdyuchenkoAN.pdf CV long version'
+            className='nav-link'
+          > long </NavLink>
           version of my CV. And you can see more information in
-          <Link to='/about/'  title='About page'> about page</Link>
+          <Link to='/about/' title='Go to About page'> about page</Link>
           .
         </main>
         <Footer />
