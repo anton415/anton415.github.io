37537d4fa451a834fd8b6e80704e8aa492359f81
Anton Serdyuchenko <anton415@gmail.com>
2019-04-24 05:40:58 +0300

    Migrate styles from index.css to Menu component.

--- src/components/menu/MenuComponent.js
+++ src/components/menu/MenuComponent.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class MenuComponent extends Component {
 
   render() {
 
     return (
-      <nav className="menuComponent">
-        <Link className="link" to="/">
+      <nav className="menuComponent" style={styles.menuComponent}>
+        <Link className="link" to="/" style={styles.link}>
             Home
         </Link>
-        <Link className="link" to="/about/">
+        <Link className="link" to="/about/" style={styles.link}>
             About
         </Link>
       </nav>
     )
+  }
+}
+
+const styles = {
+  menuComponent: {
+    backgroundColor: '#333',
+    display: 'flex',
+    listStyleType: 'none',
+    margin: 0,
+    overflow: 'hidden',
+    padding: 0
+  },
+  link: {
+    color: 'white',
+    display: 'block',
+    padding: '14px 16px',
+    textAlign: 'center',
+    textDecoration: 'none'
   }
 }
 
