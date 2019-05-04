49e2c84a56be2278aab4abff034bce722c945e58
Anton Serdyuchenko <anton415@gmail.com>
2019-05-04 15:06:45 +0300

    Add container from Semantic UI.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,6 +5,7 @@ import Footer from '../footer/Footer'
 import CVShort from 'utils/pdf/SerdyuchenkoAN_CV_short_version.pdf'
 import CVLong from 'utils/pdf/SerdyuchenkoAN_CV_long_version.pdf'
 import { NavLink } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
 
 class Home extends Component {
   render() {
@@ -12,26 +13,31 @@ class Home extends Component {
       <div className="Home">
         <main className='hello-words' role='main'>
           <Description />
-          <br />
-          You can download
-          <NavLink
-            to={CVShort}
-            target='_blank'
-            download='SerdyuchenkoAN'
-            title='Download SerdyuchenkoAN.pdf CV short version'
-            className='nav-link'
-          >  short  </NavLink>
-          or
-          <NavLink
-            to={CVLong}
-            target='_blank'
-            download='SerdyuchenkoAN'
-            title='Download SerdyuchenkoAN.pdf CV long version'
-            className='nav-link'
-          > long </NavLink>
-          version of my CV. And you can see more information in
-          <Link to='/about/' title='Go to About page'> about page</Link>
-          .
+          <Container textAlign='center'>
+            <p>
+              <br />
+              You can download
+              <NavLink
+                to={CVShort}
+                target='_blank'
+                download='SerdyuchenkoAN'
+                title='Download SerdyuchenkoAN.pdf CV short version'
+                className='nav-link'
+              >  short  </NavLink>
+              or
+              <NavLink
+                to={CVLong}
+                target='_blank'
+                download='SerdyuchenkoAN'
+                title='Download SerdyuchenkoAN.pdf CV long version'
+                className='nav-link'
+              > long </NavLink>
+              version of my CV. And you can see more information in
+              <Link to='/about/' title='Go to About page'> about page</Link>
+              .
+            </p>
+          </Container>
+
         </main>
         <Footer />
       </div>
