4d2cd9fc115597a3eb839a93aa565b550a1cddbb
Anton Serdyuchenko <anton415@gmail.com>
2019-05-19 11:12:07 +0300

    Add card and label in Skills page.

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
+import { Card, Label } from 'semantic-ui-react'
 
 class Skills extends Component {
   render() {
+    const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'React Native', 'Git', 'PWA', 'Java', 'SQL']
+
     return (
-      <div className="section">
-        <h3>Skills</h3>
-        <div className="skills card">
-          <span className="skill">React</span>
-          <span className="skill">Redux</span>
-          <span className="skill">React Native</span>
-          <span className="skill">Git</span>
-          <span className="skill">PWA</span>
-          <span className="skill">Java</span>
-          <span className="skill">SQL</span>
-        </div>
-      </div>
+      <Card fluid centered>
+        <Card.Content header='Skills' />
+        <Card.Content>
+          {skills.map(skill => (
+            <Label key={skill} size='medium'>
+              {skill}
+            </Label>
+          ))}
+        </Card.Content>
+      </Card>
     )
   }
 }
