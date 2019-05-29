37fd0f078865745a804104699b6fcea86f3e2dbf
Anton Serdyuchenko <anton415@gmail.com>
2019-05-29 07:19:24 +0300

    Change from my syles to Semantic UI.

--- src/components/about/Description.js
+++ src/components/about/Description.js
@@ -1,54 +1,33 @@
 import React, { Component } from 'react'
 import Avatar from 'utils/images/avatar.jpg'
-import { Container } from 'semantic-ui-react'
+import { Card, Item } from 'semantic-ui-react'
 
 class Description extends Component {
   render() {
     return (
-      <div className="description" style={styles.description}>
-          <div className='introAvatar' style={styles.introAvatar}>
-            <img className='avatar' src={Avatar} alt='Avatar' style={styles.avatar}/>
-          </div>
-
-          <Container>
-            <h3 className='helloWords' style={styles.helloWords}>
-              Hello, my name Anton Serdyuchenko.
-            </h3>
-            <p>
-              I work as a Full stack developer at the Central Bank.
-              I use the following technologies: JSP, JS, Java, Spring, Hibernate, SQL, Git.
-            </p>
-            <p>
-              I independently study the following technologies: React, Redux, Webpack, PWA.
-            </p>
-          </Container>
-
-
-      </div>
+      <Card centered fluid>
+        <Card.Content header='Experience' />
+        <Card.Content>
+          <Item.Group>
+            <Item>
+              <Item.Image size='tiny' src={Avatar} />
+              <Item.Content>
+                <Item.Header>Hello, my name Anton Serdyuchenko.</Item.Header>
+                <Item.Description>
+                  <p>
+                    I work as a Full stack developer at the Central Bank.
+                    I use the following technologies: JSP, JS, Java, Spring, Hibernate, SQL, Git.
+                  </p>
+                  <p>
+                    I independently study the following technologies: React, Redux, Webpack, PWA.
+                  </p>
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Card.Content>
+      </Card>
     )
-  }
-}
-
-const styles = {
-  description: {
-    display: 'flex',
-    padding: 'inherit',
-    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-    transition: '0.3s'
-  },
-  introAvatar: {
-    margin: '1%'
-  },
-  avatar: {
-    borderRadius: '50%',
-    width: '100px'
-  },
-  introDescription: {
-    margin: 'auto',
-    textAlign: '-webkit-center'
-  },
-  helloWords: {
-    textAlign: 'center'
   }
 }
 
