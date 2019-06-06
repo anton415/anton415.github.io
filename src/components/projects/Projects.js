884629f9503b94a5730a4709afd5ee766770c94f
Anton Serdyuchenko <anton415@gmail.com>
2019-06-06 06:24:53 +0300

    Add second project.

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Container, Divider } from 'semantic-ui-react';
 import { Card, Image } from 'semantic-ui-react'
 import rqgLogo from 'utils/images/project-images/random-quote-generator.png'
+import lpafLogo from 'utils/images/project-images/list-pagination-and-filtering.png'
 
 class Projects extends Component {
   render() {
@@ -17,27 +18,50 @@ class Projects extends Component {
           <p>For some of my projects, I have live version.</p>
         </Container>
         <Divider />
-        <Card>
-          <Image
-            src={rqgLogo}
-            ui={false}
-            href='http://serdyuchenko.com/random-quote-generator/'
-          />
-          <Card.Content>
-            <Card.Header>Random quote generetor</Card.Header>
-            <Card.Meta>
-              <span className='date'>Since 24 May 2019</span>
-            </Card.Meta>
-            <Card.Description>
-              A random quote generetor. First project from TreeHouse.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra textAlign='center'>
-            <a href='https://github.com/anton415/random-quote-generator'>
-              Source code
-            </a>
-          </Card.Content>
-        </Card>
+        <Card.Group>
+          <Card>
+            <Image
+              src={rqgLogo}
+              ui={false}
+              href='http://serdyuchenko.com/random-quote-generator/'
+            />
+            <Card.Content>
+              <Card.Header>Random quote generetor</Card.Header>
+              <Card.Meta>
+                <span className='date'>Since 24 May 2019</span>
+              </Card.Meta>
+              <Card.Description>
+                A random quote generetor. First project from TreeHouse.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra textAlign='center'>
+              <a href='https://github.com/anton415/random-quote-generator'>
+                Source code
+              </a>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Image
+              src={lpafLogo}
+              ui={false}
+              href='http://serdyuchenko.com/list-pagination-and-filtering/'
+            />
+            <Card.Content>
+              <Card.Header>List pagination and filtering</Card.Header>
+              <Card.Meta>
+                <span className='date'>Since 5 June 2019</span>
+              </Card.Meta>
+              <Card.Description>
+                List pagination and filtering. Second project from TreeHouse.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra textAlign='center'>
+              <a href='https://github.com/anton415/list-pagination-and-filtering'>
+                Source code
+              </a>
+            </Card.Content>
+          </Card>
+        </Card.Group>
       </Container>
     )
   }
