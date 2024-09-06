ac6e685bd98221479da43401b2781067d8433008
Anton Serdyuchenko <anton415@gmail.com>
2024-09-06 10:17:00 +0300

    Updating contacts. - Updating icons. Made them colorful. - Removing twitter.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import XIcon from '@mui/icons-material/X';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import EmailIcon from '@mui/icons-material/Email';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -16,6 +12,9 @@ import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Divider from '@mui/material/Divider';
 import ListItemButton from '@mui/material/ListItemButton';
 import CaraIcon from './static/icons/cara-icon.png';
+import MailIcon from './static/icons/mail-logo.png';
+import InstagramIcon from './static/icons/instagram-logo.png';
+import PinterestIcon from './static/icons/pinterest-logo.png';
 
 export default function Contact() {
   return (
@@ -35,31 +34,22 @@ export default function Contact() {
       <ListItem disablePadding>
         <ListItemButton component="a" href="https://www.instagram.com/anton415226/">
           <ListItemAvatar>
-            <Avatar>
-              <InstagramIcon />
-            </Avatar>
+             <Avatar
+               alt="Instagram"
+               src={InstagramIcon}
+             />
           </ListItemAvatar>
           <ListItemText primary="Instagram" secondary="More of my work here." />
         </ListItemButton>
       </ListItem>
       <Divider />
       <ListItem disablePadding>
-        <ListItemButton component="a" href="https://twitter.com/anton415226">
-          <ListItemAvatar>
-            <Avatar>
-              <XIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="X" secondary="Art related news." />
-        </ListItemButton>
-      </ListItem>
-      <Divider />
-      <ListItem disablePadding>
         <ListItemButton component="a" href="https://pin.it/62NObwbtT">
           <ListItemAvatar>
-            <Avatar>
-              <PinterestIcon />
-            </Avatar>
+            <Avatar
+              alt="Pinterest"
+              src={PinterestIcon}
+            />
           </ListItemAvatar>
           <ListItemText primary="Pinterest" secondary="References and inspiration map." />
         </ListItemButton>
@@ -68,9 +58,10 @@ export default function Contact() {
       <ListItem disablePadding>
         <ListItemButton component="a" href="mailto:anton415@gmail.com">
           <ListItemAvatar>
-            <Avatar>
-              <EmailIcon />
-            </Avatar>
+            <Avatar
+              alt="Mail"
+              src={MailIcon}
+            />
           </ListItemAvatar>
           <ListItemText primary="Email" secondary="Reach out with any questions!" />
         </ListItemButton>
